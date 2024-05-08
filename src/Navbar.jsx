3871a3538b33081573ed04a327b3adc8d5cbceea
eenglesson <eliasenglesson00@gmail.com>
2024-05-08 05:24:53 +0200

    added code

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -1,7 +1,14 @@
 import React from 'react';
 import { navItems } from './premadeList';
+import { useState } from 'react';
 
 function Navbar() {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsOpen(!isOpen);
+  };
+
   return (
     <div>
       <nav>
@@ -16,6 +23,44 @@ function Navbar() {
           ))}
         </div>
       </nav>
+      <button className='relative group' onClick={toggleMenu}>
+        <div className='relative flex overflow-hidden items-center justify-center rounded-full w-[50px] h-[50px]'>
+          <div className='flex flex-col justify-between w-[28px] h-[20px] transform transition-all duration-300 origin-center'>
+            <div
+              className={`bg-white h-[2px] w-7 rounded transform transition-all duration-300 origin-left ${
+                isOpen ? 'translate-x-10' : ''
+              }`}
+            ></div>
+            <div
+              className={`bg-white h-[2px] w-7 rounded transform transition-all duration-300 ${
+                isOpen ? 'translate-x-10 delay-75' : ''
+              }`}
+            ></div>
+            <div
+              className={`bg-white h-[2px] w-7 rounded transform transition-all duration-300 origin-left ${
+                isOpen ? 'translate-x-10 delay-150' : ''
+              }`}
+            ></div>
+
+            <div
+              className={`absolute items-center justify-between rounded transform transition-all duration-500 top-2.5 -translate-x-10 ${
+                isOpen ? 'translate-x-0 flex w-12' : 'flex w-0'
+              }`}
+            >
+              <div
+                className={`absolute bg-white h-[2px] rounded w-[28px] transform transition-all duration-500 rotate-0 delay-300 ${
+                  isOpen ? 'rotate-45' : ''
+                }`}
+              ></div>
+              <div
+                className={`absolute bg-white h-[2px] rounded w-[28px] transform transition-all duration-500 -rotate-0 delay-300 ${
+                  isOpen ? '-rotate-45' : ''
+                }`}
+              ></div>
+            </div>
+          </div>
+        </div>
+      </button>
     </div>
   );
 }
